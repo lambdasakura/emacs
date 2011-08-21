4eb824e16a4a30b015280ed12899ce12d031fe1a
lambda_sakura <lambda.sakura@gmail.com>
2011-08-21 20:02:31 +0900

    emacs-w3mを更新

--- init.el
+++ init.el
@@ -151,22 +151,22 @@
 			       'append)
 	     (add-to-list 'default-frame-alist '(font . "fontset-menlomarugo"))))))
 
-(custom-set-faces
- '(default ((t (:inherit nil 
-			 :stipple nil
-			 :background "black"
-			 :foreground "#cccccc" 
-			 :inverse-video nil
-			 :box nil 
-			 :strike-through nil 
-			 :overline nil 
-			 :underline nil 
-			 :slant normal 
-			 :weight normal
-			 :height 122 
-			 :width normal
-			 :foundry "unknown" 
-			 :family "TakaoExゴシック")))))
+;; (custom-set-faces
+;;  '(default ((t (:inherit nil 
+;; 			 :stipple nil
+;; 			 :background "black"
+;; 			 :foreground "#cccccc" 
+;; 			 :inverse-video nil
+;; 			 :box nil 
+;; 			 :strike-through nil 
+;; 			 :overline nil 
+;; 			 :underline nil 
+;; 			 :slant normal 
+;; 			 :weight normal
+;; 			 :height 122 
+;; 			 :width normal
+;; 			 :foundry "unknown" 
+;; 			 :family "TakaoExゴシック")))))
 
 ;; その他の設定
 (custom-set-variables
@@ -175,6 +175,19 @@
  '(safe-local-variable-values (quote ((package . asdf))))
  '(show-paren-mode t))
 
+;;
+(add-hook 'speedbar-mode-hook
+          '(lambda ()
+             (speedbar-add-supported-extension '("js" "as" "html" "css" "php" "lisp"))))
 
 
 
+;; Backslashes
+(define-key global-map [165] nil)
+(define-key global-map [67109029] nil)
+(define-key global-map [134217893] nil)
+(define-key global-map [201326757] nil)
+(define-key function-key-map [165] [?\\])
+(define-key function-key-map [67109029] [?\C-\\])
+(define-key function-key-map [134217893] [?\M-\\])
+(define-key function-key-map [201326757] [?\C-\M-\\])
