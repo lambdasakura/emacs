803f6ee002794378475bbe5e8841540cd697afef
lambda_sakura <lambda.sakura@gmail.com>
2016-02-18 13:25:31 +0900

    Fix helm setting

--- conf/init-helm.el
+++ conf/init-helm.el
@@ -18,21 +18,27 @@
          (,(kbd "M-y")   helm-show-kill-ring)
          (,(kbd "M-z")   helm-do-grep)
          (,(kbd "C-S-;") helm-descbinds)
-        )))
+         )))
   (loop for (key func) in key-and-func
         do (global-set-key key func)))
- 
+
 (global-set-key (kbd "C-M-o") 'helm-occur) ;; helm-occurの起動
 (global-set-key (kbd "C-x C-r") 'helm-recentf)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x rl") 'helm-bookmarks)
-(global-set-key (kbd "C-M-o") 'helm-occur) 
-(define-key isearch-mode-map (kbd "C-o") 'helm-occur-from-isearch) ;; isearchからhelm-occurを起動
-(define-key helm-map (kbd "C-c C-a") 'all-from-helm-occur) ;; helm-occurからall-extに受け渡し
+(global-set-key (kbd "C-M-o") 'helm-occur)
+
+;; isearchからhelm-occurを起動
+(define-key isearch-mode-map (kbd "C-o") 'helm-occur-from-isearch)
+;; helm-occurからall-extに受け渡し
+(define-key helm-map (kbd "C-c C-a") 'all-from-helm-occur)
 
 ;; 自動補完を無効
 (custom-set-variables '(helm-ff-auto-update-initial-value nil))
-;; C-hでバックスペースと同じように文字を削除  
-(define-key helm-c-read-file-map (kbd "C-h") 'delete-backward-char)
-;; TABで任意補完。選択肢が出てきたらC-nやC-pで上下移動してから決定することも可能
-(define-key helm-c-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 
+;; For find-file etc.
+(define-key helm-read-file-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+
+;; For helm-find-files etc.
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
