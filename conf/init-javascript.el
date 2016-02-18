e49a76ca39de9a9a8c5ff9043ee823f257882e43
lambda_sakura <lambda.sakura@gmail.com>
2016-02-18 13:25:56 +0900

    js2-mode

--- conf/init-javascript.el
+++ conf/init-javascript.el
@@ -1,2 +1,5 @@
 ;; javascript
-(autoload 'js3-mode "js3" nil t)
+(require 'js2-mode)
+(autoload 'js2-mode "js2" nil t)
+;; (flycheck-add-mode 'javascript-eslint 'js2-jsx-mode)
+;; (add-hook 'js2-jsx-mode-hook 'flycheck-mode)
