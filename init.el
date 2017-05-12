e2566d9e5adb26657867302ae7239f3788230121
lambda_sakura <lambda.sakura@gmail.com>
2017-05-12 15:33:26 +0900

    Add auto generated comments

--- init.el
+++ init.el
@@ -4,6 +4,13 @@
 
 ;;; Code:
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defun add-to-load-path (&rest paths)
   (mapc #'(lambda (path)
             (add-to-list 'load-path path))
@@ -33,7 +40,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
- '(helm-ff-auto-update-initial-value nil))
+ '(helm-ff-auto-update-initial-value nil)
+ '(package-selected-packages
+   (quote
+    (exec-path-from-shell flycheck-pos-tip flycheck go-eldoc go-autocomplete go-mode popwin evil ac-slime twittering-mode ac-cider cider clojure-mode ruby-electric ruby-block jedi ensime scala-mode yaml-mode google-c-style haskell-mode js2-mode web-mode sass-mode php-mode inf-ruby neotree all-ext w3m color-moccur deferred yasnippet yatex quickrun powerline helm-c-yasnippet helm-c-moccur helm-gtags helm-descbinds helm atom-one-dark-theme auto-complete markdown-mode dired+ init-loader bind-key use-package slack))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
