1f5a13d3f17985e152fc693318c2b045672f4221
lambda_sakura <lambda.sakura@gmail.com>
2017-07-10 22:22:23 +0900

    update indentation

--- inits/30-javascript.el
+++ inits/30-javascript.el
@@ -52,6 +52,10 @@
               (setq web-mode-code-indent-offset 2)
               (setq web-mode-sql-indent-offset 2)
               (setq indent-tabs-mode nil)
+              (add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
+              (add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
+              (add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
+              (add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil))
               (setq tab-width 2))))
 
 (provide '30-javascript)
